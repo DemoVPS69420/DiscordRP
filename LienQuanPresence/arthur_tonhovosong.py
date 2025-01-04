69dd2d3484c6ff6f45e73429b46ce58bc949a4ff
Kaden Megarimi <kadenmegarimi@gmail.com>
2025-01-04 21:17:21 +0700

    Create arthur_tonhovosong.py

---
+++ LienQuanPresence/arthur_tonhovosong.py
@@ -0,0 +1,21 @@
+import os
+from pypresence import Presence
+import time
+
+print("Running...(arthur_tonhovosong.py)")
+
+RPC = Presence(1152566755357626388)
+RPC.connect()
+
+RPC.update(
+            details="Đấu xếp hạng",
+            state="Arthur Tôn Hổ Vô Song (Mythic)",
+            start=time.time(),
+            large_image=("https://raw.githubusercontent.com/DemoVPS69420/DiscordRP/refs/heads/main/lienquanimage/Full_53111_EDIT_1734953040847.jpg"), 
+            large_text="Arthur Tôn Hổ Vô Song",
+            small_image=("https://raw.githubusercontent.com/DemoVPS69420/DiscordRP/refs/heads/main/lienquanimage/1110855395368509520.png"),
+            small_text="Cao Thủ",
+            buttons=[
+                {"label": "Server Discord của Garena LQM", "url": "https://discord.gg/lqm"}]
+          )
+time.sleep(999999)
